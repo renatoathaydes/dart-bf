502b3309c4450b17bd369cd162fe909d132d35e0
renatoathaydes <renatoathaydes@hotmail.com>
2019-07-06 11:51:09 +0200

    CLI proper error handling.

--- bin/dart-bf.dart
+++ bin/dart-bf.dart
@@ -4,5 +4,10 @@ import 'dart:io';
 import 'package:dart_bf/dart_bf.dart';
 
 main(List<String> args) async {
+  if (args.length != 1) {
+    stderr.writeln("Error: expected one argument, given ${args.length}\n"
+        "Usage: dart-bf <bf-file>");
+    exit(2);
+  }
   execute(await File(args[0]).readAsString(encoding: ascii));
 }
